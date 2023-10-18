3143d4574ede3ba00925f687dcd39e9479949b1b
Alula Mekonen <alulamoke09@gmail.com>
2023-10-18 23:33:29 +0300

    addCast

--- admin/src/components/AddCast.jsx
+++ admin/src/components/AddCast.jsx
@@ -1,29 +1,21 @@
-import React, { useState, useCallback } from 'react';
-import styled from 'styled-components';
+import { useState, useRef } from 'react';
+import { IoCreateOutline } from 'react-icons/io5';
 
-// Redux
-import { useDispatch } from 'react-redux';
-import { createCast } from '../redux/actions/cast.action';
+// hooks
+import { useCreateCast } from '../hooks/useCast';
+import { useFormik } from 'formik';
+import { castSchema } from '../schemas';
 
 // Components
 import Button from './Button';
 import Modal from './Modal';
 
-const FormWrapper = styled.form`
-  display: flex;
-  flex-direction: column;
-  max-width: 100%;
-  margin: auto;
-`;
-
 const AddCast = () => {
-  const dispatch = useDispatch();
+  const createCastMutation = useCreateCast();
+
   const [modalOpen, setModalOpen] = useState(false);
-  const [fullname, setFullName] = useState('');
   const [image, setImage] = useState(null);
-  const [birthday, setBirthday] = useState('');
-  const [deathday, setDeathday] = useState('');
-  const [biography, setBiography] = useState('');
+  const fileRef = useRef(null);
 
   const handleOpen = () => {
     setModalOpen(true);
@@ -33,113 +25,123 @@ const AddCast = () => {
     setModalOpen(false);
   };
 
-  const handleEditImage = useCallback(() => {
-    const fileInput = document.getElementById('imageInput');
-    fileInput.click();
-  }, []);
+  const formAction = useFormik({
+    initialValues: {
+      fullname: '',
+      birthday: '',
+      deathday: '',
+      biography: '',
+    },
+    validationSchema: castSchema,
+    onSubmit: (values) => {
+      const formData = new FormData();
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    const formData = new FormData();
-    formData.append('fullname', fullname);
-    formData.append('imageurl', image);
-    formData.append('birthday', birthday);
-    if (deathday) formData.append('deathday', deathday);
-    formData.append('biography', biography);
-    dispatch(createCast(formData)).then(() => {
-      handleClose();
-    });
-  };
+      formData.append('fullname', values.fullname);
+      formData.append('imageurl', image);
+      formData.append('birthday', values.birthday);
+      if (values.deathday) formData.append('deathday', values.deathday);
+      formData.append('biography', values.biography);
+
+      createCastMutation.mutate(formData);
+    },
+  });
 
   return (
     <>
       <Button
         title="Add Cast"
+        Icon={IoCreateOutline}
         left
-        icon="plus"
-        style={{ marginLeft: '1.5rem' }}
         onClick={handleOpen}
       />
       <Modal open={modalOpen} title="Add Cast" onClose={handleClose}>
-        <FormWrapper noValidate onSubmit={(e) => e.preventDefault()}>
-          <label htmlFor="fullname">* Fullname</label>
+        <form
+          noValidate
+          onSubmit={formAction.handleSubmit}
+          className="max-w-full flex gap-4 flex-col mx-auto"
+        >
+          <div className="form-group">
+            <label htmlFor="fullname">* Fullname</label>
+            <input
+              id="fullname"
+              name="fullname"
+              type="text"
+              value={formAction.values.fullname}
+              onChange={formAction.handleChange}
+              onBlur={formAction.handleBlur}
+            />
+            {formAction.errors.fullname && formAction.touched.fullname && (
+              <p className="form-error">{formAction.errors.fullname}</p>
+            )}
+          </div>
+          <label htmlFor="imageInput">* Image</label>
           <input
-            id="fullname"
-            name="fullname"
-            type="text"
-            value={fullname}
-            onChange={(e) => setFullName(e.target.value)}
-            placeholder="Name"
+            ref={fileRef}
+            name="image"
+            accept="image/*"
+            type="file"
+            hidden="hidden"
+            onChange={(e) => setImage(e.target.files[0])}
           />
-          {fullname && (
-            <>
-              <label htmlFor="imageInput">* Image</label>
-              <input
-                id="imageInput"
-                name="image"
-                accept="image/*"
-                type="file"
-                hidden="hidden"
-                onChange={(e) => setImage(e.target.files[0])}
-              />
-              <Button
-                title={image ? 'Edit Image' : 'Add Image'}
-                color={image ? '#00b100' : 'var(--color-primary-dark)'}
-                icon={image ? 'check-circle' : 'edit'}
-                left
-                solid={image}
-                onClick={handleEditImage}
-                style={{ margin: '1rem 0' }}
-              />
-            </>
-          )}
-          {image && (
-            <>
-              <label htmlFor="birthday">* Birthday</label>
-              <input
-                id="birthday"
-                name="birthday"
-                type="date"
-                value={birthday}
-                onChange={(e) => setBirthday(e.target.value)}
-              />
-            </>
-          )}
-          {birthday && (
-            <>
-              <label htmlFor="deathday">* Deathday</label>
-              <input
-                id="deathday"
-                name="deathday"
-                type="date"
-                value={deathday}
-                onChange={(e) => setDeathday(e.target.value)}
-              />
-            </>
-          )}
-          {(birthday || deathday) && (
-            <>
-              <label htmlFor="biography">* Biography</label>
-              <textarea
-                id="biography"
-                name="biography"
-                type="text"
-                value={biography}
-                onChange={(e) => setBiography(e.target.value)}
-                placeholder="Biography"
-              />
-            </>
-          )}
           <Button
-            title="Create Cast"
+            title={image ? 'Edit Image' : 'Add Image'}
+            color={image ? '#00b100' : 'var(--color-primary-dark)'}
+            Icon={IoCreateOutline}
             left
-            icon="plus"
-            solid
-            style={{ marginTop: '2rem' }}
-            onClick={handleSubmit}
-            disabled={!fullname || !image || !birthday || !biography}
+            solid={image}
+            onClick={fileRef.current.click()}
           />
-        </FormWrapper>
+          <div className="form-group">
+            <label htmlFor="birthday">* Birthday</label>
+            <input
+              id="birthday"
+              name="birthday"
+              type="date"
+              value={formAction.values.birthday}
+              onChange={formAction.handleChange}
+              onBlur={formAction.handleBlur}
+            />
+            {formAction.errors.birthday && formAction.touched.birthday && (
+              <p className="form-error">{formAction.errors.birthday}</p>
+            )}
+          </div>
+          <div className="form-group">
+            <label htmlFor="deathday">* Deathday</label>
+            <input
+              id="deathday"
+              name="deathday"
+              type="date"
+              value={formAction.values.deathday}
+              onChange={formAction.handleChange}
+              onBlur={formAction.handleBlur}
+            />
+            {formAction.errors.deathday && formAction.touched.deathday && (
+              <p className="form-error">{formAction.errors.deathday}</p>
+            )}
+          </div>
+          <div className="form-group">
+            <label htmlFor="biography">* Biography</label>
+            <textarea
+              id="biography"
+              name="biography"
+              type="text"
+              value={formAction.values.biography}
+              onChange={formAction.handleChange}
+              onBlur={formAction.handleBlur}
+            />
+            {formAction.errors.biography && formAction.touched.biography && (
+              <p className="form-error">{formAction.errors.biography}</p>
+            )}
+          </div>
+          <Button
+            title="Submit"
+            Icon={IoCreateOutline}
+            left
+            solid
+            className="mt-4"
+            disabled={createCastMutation.isLoading}
+          />
+        </form>
       </Modal>
     </>
   );
